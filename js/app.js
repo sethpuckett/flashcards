5b17a6a102d64c6b8f863abbdc73799ebaa539af
Seth Puckett <puckett.seth@gmail.com>
2018-10-18 11:16:32 -0500

    load cards

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
-
+  loadHeaders();
+  loadCards();
 });
 
 function loadHeaders() {
-
+  $("#header-title").text(CARDS['title']);
+  $('#card-header-left').text(CARDS['left-category']);;
+  $('#card-header-right').text(CARDS['right-category']);;
 }
 
 function loadCards() {
+  cards = CARDS['cards'];
 
+  cards.forEach(card => {
+    left = card['left'];
+    right = card['right'];
+    notes = card['notes'];
+
+    var tr = `<tr><td class='left'>${left}</td><td class='right'>${right}</td><td class='notes'>${notes}</td></tr>`;
+    $('#card-table > tbody:last-child').append(tr);
+  });
 }
 
 function hideAll() {
