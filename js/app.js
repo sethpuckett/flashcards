4d9c735aca6db05011ccf14eeb261658b8087256
Seth Puckett <puckett.seth@gmail.com>
2018-10-19 14:46:25 -0500

    show/hide logic

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 $(document).ready(function () {
   loadHeaders();
   loadCards();
+
+  $("#btn-hide-all").on('click', hideAll);
+  $("#btn-show-all").on('click', showAll);
+  $("#btn-hide-left").on('click', hideLeft);
+  $("#btn-hide-right").on('click', hideRight);
+  $(".card").on('click', toggleCard);
 });
 
 function loadHeaders() {
   $('#header-title').text(CARDS['title']);
-  $('#card-header-left').text(CARDS['left-category']);;
-  $('#card-header-right').text(CARDS['right-category']);;
+  $('#card-header-left').text(CARDS['left-category']);
+  $('#card-header-right').text(CARDS['right-category']);
+  $('#btn-hide-left').text(`Hide ${CARDS['left-category']}`);
+  $('#btn-hide-right').text(`Hide ${CARDS['right-category']}`);
 }
 
 function loadCards() {
@@ -30,17 +38,21 @@ function loadCards() {
 }
 
 function hideAll() {
-
+  $('.card p').fadeOut();
 }
 
 function showAll() {
-
+  $('.card p').fadeIn();
 }
 
 function hideLeft() {
-
+  $('.card.left p').fadeOut();
 }
 
-function showLeft() {
+function hideRight() {
+  $('.card.right p').fadeOut();
+}
 
+function toggleCard() {
+  $(this).find('p').fadeToggle();
 }
