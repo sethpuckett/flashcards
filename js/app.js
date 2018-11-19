37a392e3116aa6262eeaaba4bb411b9a5638bf72
Seth Puckett <puckett.seth@gmail.com>
2018-11-19 11:04:51 -0600

    add keyboard shortcuts

--- js/app.js
+++ js/app.js
@@ -20,7 +20,132 @@ $(document).ready(function () {
 
   $('#btn-load-deck').on('click', deckSelected);
   $('#btn-shuffle').on('click', shuffleDeck);
+
+  $(document).keypress(handleKey);
 });
+
+function handleKey(e) {
+  switch(e.key) {
+    case 'a':
+      toggleSelectedLeft();
+      break;
+    case 's':
+      toggleSelectedRight();
+      break;
+    case 'd':
+      toggleSelectedNotes();
+      break;
+    case 'f':
+      ignoreAndSelectNext();
+      break;
+    case 'g':
+      undoLastIgnore();
+      break;
+    case 'e':
+      selectPreviousRow();
+      break;
+    case 'r':
+      selectNextRow();
+      break;
+    case 'q':
+      shuffleAndMoveToTop();
+      break;
+  }
+}
+
+function toggleSelectedLeft() {
+  row = getSelectedRow();
+  if (row == null) return;
+
+  toggleCard.call($(row).find('.left')[0]);
+}
+
+function toggleSelectedRight() {
+  row = getSelectedRow();
+  if (row == null) return;
+
+  toggleCard.call($(row).find('.right')[0]);
+}
+
+function toggleSelectedNotes() {
+  row = getSelectedRow();
+  if (row == null) return;
+
+  toggleCard.call($(row).find('.notes')[0]);
+}
+
+function ignoreAndSelectNext() {
+  currentRow = getSelectedRow();
+  selectNextRow();
+
+  if (currentRow != null) {
+    ignoreCard.call($(currentRow).find('.btn-ignore')[0]);
+  }
+}
+
+function undoLastIgnore() {
+  row = $('#card-table > .body.ignored > .table-row:last-child')[0];
+  if (row == null) return;
+  unignoreCard.call($(row).find('.btn-unignore')[0], getSelectedRow());
+  selectPreviousRow();
+}
+
+function shuffleAndMoveToTop() {
+  shuffleDeck();
+  selectFirstRow();
+  window.scrollTo(0, 0);
+}
+
+function selectNextRow() {
+  row = getSelectedRow();
+
+  if (row == null) {
+    var firstRow = $('#card-table > .body.active').children()[0];
+    $(firstRow).addClass('selected');
+  } else {
+    var nextRow = $(row).next()[0];
+    if (nextRow != null) {
+      $(row).removeClass('selected');
+      $(nextRow).addClass('selected');
+    }
+  }
+}
+
+function selectFirstRow() {
+  $('.table-row').removeClass('selected');
+  var firstRow = $('#card-table > .body.active').children()[0];
+  $(firstRow).addClass('selected');
+}
+
+function selectRow() {
+  row = $(this).parents('.table-row')[0];
+  $('.table-row').removeClass('selected');
+  $(row).addClass('selected');
+}
+
+function selectPreviousRow() {
+  row = getSelectedRow();
+
+  if (row == null) {
+    var firstRow = $('#card-table > .body.active').children()[0];
+    $(firstRow).addClass('selected');
+  } else {
+    var prevRow = $(row).prev()[0];
+    if (prevRow != null) {
+      $(row).removeClass('selected');
+      $(prevRow).addClass('selected');
+    }
+  }
+}
+
+function getSelectedRow() {
+  rows = $('#card-table > .body.active').find('.selected');
+  if (rows.length > 0) {
+    return rows[0];
+  }
+
+  return null;
+}
 
 function loadAllDecks(data) {
   decks = [];
@@ -94,32 +219,34 @@ function loadDeck(data, tabletop) {
 
   hideRight();
   hideNotes();
+  selectFirstRow();
 }
 
 function hideAll() {
-  $('.card p').fadeOut();
+  $('.card p').hide();
 }
 
 function showAll() {
-  $('.card p').fadeIn();
+  $('.card p').show();
 }
 
 function hideLeft() {
-  $('.card.right p').fadeIn();
-  $('.card.left p').fadeOut();
+  $('.card.right p').show()
+  $('.card.left p').hide();
 }
 
 function hideRight() {
-  $('.card.left p').fadeIn();
-  $('.card.right p').fadeOut();
+  $('.card.left p').show();
+  $('.card.right p').hide();
 }
 
 function hideNotes() {
-  $('.card.notes p').fadeOut();
+  $('.card.notes p').hide();
 }
 
 function toggleCard() {
-  $(this).find('p').fadeToggle();
+  $(this).find('p').toggle();
+  selectRow.call(this);
 }
 
 function ignoreCard() {
@@ -129,11 +256,17 @@ function ignoreCard() {
   $(this).parent().detach().appendTo('#card-table > .body.ignored');
 }
 
-function unignoreCard() {
+function unignoreCard(afterSibling) {
   $(this).text('x');
   $(this).removeClass('btn-unignore');
   $(this).addClass('btn-ignore');
-  $(this).parent().detach().appendTo('#card-table > .body.active');
+  if (afterSibling == null) {
+    $(this).parent().detach().appendTo('#card-table > .body.active');
+  } else {
+    var row = $(this).parent();
+    $(row).detach();
+    $(row).insertBefore(afterSibling);
+  }
 }
 
 function shuffleDeck() {
