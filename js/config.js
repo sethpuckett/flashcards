3190ec08d29129b3c1c1f3782b21677ffcb20c10
Seth Puckett <puckett.seth@gmail.com>
2018-11-24 16:03:55 -0600

    add config

---
+++ js/config.js
@@ -0,0 +1,7 @@
+
+// The key for the Google spreadsheet containing flashcard topics.
+const TOPIC_SPREADSHEET_KEY = '159Xdlkq_k9gr5kUt_ICNHOlVmqWXxTwxR3LBemNMKAU';
+
+// Default theme
+// Values are: light, dark
+const DEFAULT_THEME = 'dark'
